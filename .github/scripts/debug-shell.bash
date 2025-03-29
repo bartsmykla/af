8736aea0371e1f209cda35ffab2ac42f1602ba21
Bart Smykla <bartek@smykla.com>
2025-03-29 08:03:26 +0100

    chore: improve release.yaml gh workflow
    Signed-off-by: Bart Smykla <bartek@smykla.com>

---
+++ .github/scripts/debug-shell.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -z "${TUNSHELL_SECRET:-}" ]]; then
+  printf 'Missing env var: TUNSHELL_SECRET\n' >&2
+  exit 1
+fi
+
+url_init="${URL_INIT:-https://lets.tunshell.com/init.sh}"
+tunshell_relay="${TUNSHELL_RELAY:-eu.relay.tunshell.com}"
+url_api_sessions="${URL_API_SESSIONS:-https://$tunshell_relay/api/sessions}"
+
+tunshell_keys="$(curl --silent --show-error --fail --request POST "$url_api_sessions")"
+peer1_key="$(echo "$tunshell_keys" | jq --raw-output .peer1_key)"
+peer2_key="$(echo "$tunshell_keys" | jq --raw-output .peer2_key)"
+
+printf '# Debug Shell:\nsh %s L %s %s %s\n' \
+  "\$(curl --silent --show-error --fail $url_init | psub)" \
+  "$peer2_key" \
+  "\$TUNSHELL_SECRET" \
+  "$tunshell_relay"
+
+curl --silent --show-error --fail "$url_init" | sh -s -- T "$peer1_key" "$TUNSHELL_SECRET" "$tunshell_relay"
